3333c681c36f2be5ed09bfc700289519b9499f17
Finn Holden <finnholden15@gmail.com>
2023-05-10 13:06:08 +0800

    feat: update erc20 deployment

--- deploy/deploy-erc.ts
+++ deploy/deploy-erc.ts
@@ -2,6 +2,7 @@ import {utils, Wallet} from "zksync-web3";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {Deployer} from "@matterlabs/hardhat-zksync-deploy";
 import * as dotenv from "dotenv";
+import {ethers} from "hardhat";
 
 dotenv.config()
 export default async function(hre: HardhatRuntimeEnvironment) {
@@ -9,6 +10,6 @@ export default async function(hre: HardhatRuntimeEnvironment) {
   const wallet = new Wallet(process.env.ZKSYNC_PK as string);
   const deployer = new Deployer(hre, wallet);
   const artifact = await deployer.loadArtifact("BulletERC20");
-  const contract = await deployer.deploy(artifact, ["Bullet", "BLT"]);
+  const contract = await deployer.deploy(artifact, ["Bullets", "BLT", ethers.utils.parseEther((10_000_000).toString())]);
   console.log("deployed to:", contract.address);
 }
--- scripts/deploy-erc.ts
+++ scripts/deploy-erc.ts
@@ -2,7 +2,7 @@ import {ethers} from "hardhat";
 
 async function main() {
   const ERC = await ethers.getContractFactory("BulletERC20");
-  const erc = await ERC.deploy("TestB", "TSTB", '10000000');
+  const erc = await ERC.deploy("Bullets", "BLT", ethers.utils.parseEther((10_000_000).toString()));
   await erc.deployed();
 
   console.log("ERC20 deployed to:", erc.address);
